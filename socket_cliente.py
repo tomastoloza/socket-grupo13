6eabf827ac48f4d13d205cb4a6b34e6cf4839629
tomastoloza <tomasezequieltoloza@gmail.com>
2020-09-23 23:24:16 -0300

    Add console output files

--- socket_cliente.py
+++ socket_cliente.py
@@ -4,8 +4,8 @@ socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_cliente.connect(("127.0.0.1", 7777))
 
 while True:
-    mensaje = str(input(">>")).encode('utf-8')
-    socket_cliente.send(mensaje)
+    mensaje = str(input(">>"))
+    socket_cliente.send(mensaje.encode('utf-8'))
 
     recibido = socket_cliente.recv(1024)
     print("Recibido: ", recibido)
